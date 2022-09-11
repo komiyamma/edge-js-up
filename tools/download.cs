8bb768fd9ac2f4f826b8380c461aceadd57939ad
komiyamma <komiyama0629@gmail.com>
2022-09-12 04:48:17 +0900

    セキュリティでダウンロードできないので、できるようにする。

--- tools/download.cs
+++ tools/download.cs
@@ -8,7 +8,7 @@ class Program
         if (args.Length != 2) {
             throw new InvalidOperationException("Usage: download.exe <url> <file>");
         }
-
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12 | SecurityProtocolType.Tls13;
         Console.WriteLine("Downloading " + args[0] + " to " + args[1] + "...");
         var client = new WebClient();
         client.DownloadFile(args[0], args[1]);
