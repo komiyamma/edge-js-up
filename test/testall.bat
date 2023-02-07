20d9647d35904b4cfd735c39affb6ab214417748
SkeletonGamer <skeletongamer@me.com>
2023-02-07 15:59:09 +0100

    Update for Node 14.21.2 / 16.19.0 / 18.13.0 / 18.14.0

--- test/testall.bat
+++ test/testall.bat
@@ -10,17 +10,17 @@ if "%1"=="" set run64=Y
 if "%1"=="x64" set run64=Y
 
 if "%run32%"=="Y" (
-	call "%SELF%\test.bat" ia32 19.3.0
-	call "%SELF%\test.bat" ia32 18.12.1
+	call "%SELF%\test.bat" ia32 19.6.0
+	call "%SELF%\test.bat" ia32 18.14.0
 	call "%SELF%\test.bat" ia32 17.9.1
-	call "%SELF%\test.bat" ia32 16.18.1
-	call "%SELF%\test.bat" ia32 14.21.1
+	call "%SELF%\test.bat" ia32 16.19.0
+	call "%SELF%\test.bat" ia32 14.21.2
 )
 
 if "%run64%"=="Y" (
-	call "%SELF%\test.bat" x64 19.3.0
-	call "%SELF%\test.bat" x64 18.12.1
+	call "%SELF%\test.bat" x64 19.6.0
+	call "%SELF%\test.bat" x64 18.14.0
 	call "%SELF%\test.bat" x64 17.9.1
-	call "%SELF%\test.bat" x64 16.18.1
-	call "%SELF%\test.bat" x64 14.21.1
+	call "%SELF%\test.bat" x64 16.19.0
+	call "%SELF%\test.bat" x64 14.21.2
 )
